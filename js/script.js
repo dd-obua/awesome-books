bc318500bf5e02674a94e665642ff0b169ca002d
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-13 13:27:54 +0300

    Create functions for adding books to the collection

--- js/script.js
+++ js/script.js
@@ -7,3 +7,37 @@ const inputAuthor = select('.author');
 const btnSubmit = select("button[type='submit']");
 
 const bookList = [];
+
+const alreadyAdded = () => {
+  return (
+    bookList.length > 0 &&
+    bookList.some((book) => book.title === inputTitle.value && book.author === inputAuthor.value)
+  );
+};
+
+const clearInput = () => {
+  inputTitle.value = '';
+  inputAuthor.value = '';
+};
+
+const addBook = (title, author) => {
+  try {
+    const empty = inputTitle.value === '' || inputAuthor.value === '';
+    if (empty) throw new Error("Both title and author can't be empty.");
+
+    if (alreadyAdded()) throw new Error('Book already added');
+
+    bookList.push({
+      title: inputTitle.value,
+      author: inputAuthor.value,
+    });
+
+    console.log(bookList);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    clearInput();
+  }
+};
+
+btnSubmit.addEventListener('click', addBook);
