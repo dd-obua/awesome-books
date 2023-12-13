37380d5f30d655bedeb9ea95f853d5fad70e2343
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-13 13:57:02 +0300

    Display book collection on the page

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const select = (selector) => document.querySelector(selector);
 const inputTitle = select('.title');
 const inputAuthor = select('.author');
 const btnSubmit = select("button[type='submit']");
+const bookListElem = select('.book-list');
 
 const bookList = [];
 
@@ -33,11 +34,34 @@ const addBook = (title, author) => {
     });
 
     console.log(bookList);
+    displayBookList(bookList);
   } catch (error) {
     console.error(error);
   } finally {
     clearInput();
   }
+};
+
+const clearContent = () => (bookListElem.innerHTML = '');
+
+const displayBookList = (list) => {
+  if (list.length < 0) return;
+  clearContent();
+
+  const bookMarkup = list
+    .map(
+      (book, index) =>
+        `
+          <li>
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <button class="remove-btn" data-index="${index}">Remove</button>
+          </li>
+        `
+    )
+    .join('');
+
+  bookListElem.insertAdjacentHTML('beforeend', bookMarkup);
 };
 
 btnSubmit.addEventListener('click', addBook);
