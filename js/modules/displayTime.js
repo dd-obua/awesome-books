79cadf07dc50efba9ec115c3589012924d26db25
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-15 13:24:45 +0300

    Implement time display using luxon library

--- js/modules/displayTime.js
+++ js/modules/displayTime.js
@@ -1,18 +1,7 @@
+import { DateTime } from '../../node_modules/luxon/src/luxon.js';
+
 export const displayTime = () => {
-  const currentDate = new Date();
-  // prettier-ignore
-  const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
-];
-  const monthIndex = currentDate.getMonth();
-  const dayOfMonth = currentDate.getDate();
-  const year = currentDate.getFullYear();
-  const hours = currentDate.getHours();
-  const standardHours = hours < 13 ? hours : hours % 12;
-  const minutes = currentDate.getMinutes();
-  const seconds = currentDate.getSeconds();
-  const time = `${standardHours}:${minutes}:${seconds}`;
-  const amOrPm = hours < 13 ? 'am' : 'pm';
-  return `${months[monthIndex]} ${dayOfMonth}th ${year}, ${time} ${amOrPm}`;
+  const now = DateTime.local();
+  const formattedTime = now.toFormat("MMMM d'th' yyyy, h:mm:ss a");
+  return formattedTime;
 };
